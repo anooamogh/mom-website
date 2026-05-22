fba6e5fcc28e135257059becd39eaae217af8feb
Amogh Anoo <141118934+anooamogh@users.noreply.github.com>
2026-05-21 23:44:01 -0500

    lol hi

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const paintings = [
   "paintings/starry-night.jpg",
   "paintings/olive-trees.jpg",
   "paintings/cafe-terrace-at-night.jpg",
-  "paintings/the-harvest.jpg"
+  "paintings/the-harvest.jpg",
+  "paintings/monet1.jpg",
+  "paintings/monet2.jpg",
+  "paintings/monet3.jpg",
+  "paintings/monet4.jpg",
+  "paintings/monet5.jpg",
 ];
 
-const dayIndex = Math.floor(Date.now() / 86400000) % paintings.length;
+const chicagoNow = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Chicago" }));
+const dayIndex = Math.floor(chicagoNow.getTime() / 86400000) % paintings.length;
 
 document.body.style.backgroundImage = `url('${paintings[dayIndex]}')`;
 
